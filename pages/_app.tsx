fd4bfdf243f89fd860a5aacd483119d7685f7d0a
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-01-13 12:32:00 -0300

    feat: add default page styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import 'tailwindcss/tailwind.css'
-import type { AppProps } from 'next/app'
+import 'tailwindcss/tailwind.css';
+import '../styles/global.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
