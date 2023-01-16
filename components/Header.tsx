0e81d071cdc375f86d4afdcc799bff6794531768
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-01-16 14:07:59 -0300

    chore: add Header component

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="w-screen flex justify-around items-center">
+        <div className="antialiased">
+          <h1 className="text-4xl w-40">Stoc<span className="text-red-600">X</span>cel</h1>
+          <hr className="w-25" />
+        </div>
+        <div className="p-11" />
+        <i>Ham</i>
+      </header>
+  );
+}
