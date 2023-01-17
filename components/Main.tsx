0f39f456037a90c11237f4be229469c2de72a842
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-01-17 14:36:23 -0300

    style: main component style improved

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,21 +1,39 @@
 import useObserver from "../hooks/useObeserver";
+import {BiSearchAlt} from 'react-icons/bi';
 
 export default function Main() {
   const { ref, view } = useObserver<HTMLHeadingElement>();
   const { ref: ref2, view: view2 } = useObserver<HTMLHeadingElement>();
 
   return (
-    <main className="w-screen h-screen flex-col justify-around items-center p-4">
-        <h1 ref={ref} className={ view?.isIntersecting ? 'opacity-1' : 'opacity-0' }>Ola, beleza?</h1>
-        <div className="h-24" />
-        <div className="h-24" />
-        <div className="h-24" />
-        <div className="h-24" />
-        <div className="h-24" />
-        <div className="h-24" />
-        <div className="h-24" />
-        <div className="h-24" />
-        <h1 ref={ref2} className={ view2?.isIntersecting ? 'opacity-0' : 'opacity-1' }>Ola, beleza?</h1>
+    <main
+    className="w-[90vw] h-screen flex-col justify-around items-center p-4 m-auto"
+    >
+        <div className="mb-4">
+          <h1
+            ref={ref}
+            className={ `${view?.isIntersecting ? 'animate-fadeIn' : ''} text-[17px]
+            font-[helvetica nue] text-center font-[600] text-neutral-800/[0.9]` }
+          >
+            Busque por ativos nacionais e fundos imobiliários.
+          </h1>
+        </div>
+
+        <div className="flex-col w-[100%] bg-transparent">
+          <div
+            ref={ref2}
+            className={ `${view2?.isIntersecting ? 'animate-scale' : ''}
+            cursor-pointer flex items-center border
+            border-black/[0.4] rounded-[7px]` }
+          >
+            <input
+              type="text"
+              className=" w-[300px] h-[50px] p-3 bg-transparent"
+              placeholder="Digite o código do ativo"
+            />
+            <button type="button" className="text-2xl text-gray-800 bg-transparent"><BiSearchAlt /></button>
+          </div>
+        </div>
       </main>
   );
 }
