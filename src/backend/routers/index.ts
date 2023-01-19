390632a265a7e109187dc6cb249ee57f355002a6
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-01-19 02:38:57 -0300

    feat: add api fetcher through tRPC and restructure folders

---
+++ src/backend/routers/index.ts
@@ -0,0 +1,31 @@
+import { z } from 'zod';
+import { procedure, router } from '../trpc';
+
+interface Stocks { 
+  stocks: [{ 
+    change: number;
+    close: number;
+    logo: string;
+    market_cap: number;
+    name: string;
+    sector: string;
+    stock: string;
+    volume: number;
+  }] 
+}
+
+export const appRouter = router({
+  stocks: procedure.query(async () => {
+    const response = await fetch('https://brapi.dev/api/quote/list?');
+    const data: Stocks = await response.json();
+    const sortedArr = data.stocks
+      .sort((a, b) => b.change - a.change);
+    return {
+      stocks: data.stocks,
+      sorted: sortedArr.slice(0, 9),
+    };
+  }),
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
