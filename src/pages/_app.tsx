1b9b79a81c6dc21063f08fb45e611d09d08843ce
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-01-19 02:32:19 -0300

    feat: add trpc to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import 'tailwindcss/tailwind.css';
 import '../styles/global.css';
 import type { AppProps } from 'next/app';
+import type { AppType } from 'next/app';
+import { trpc } from '../utils/trpc';
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+export default trpc.withTRPC(App);
