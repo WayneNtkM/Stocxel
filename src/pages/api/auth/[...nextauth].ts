9d15f8536ce7d8179e9cf0845d698cd69a6649a7
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-02-01 13:37:13 -0300

    fix: removed Mongo adapter

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,8 @@
 import NextAuth from "next-auth";
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
-import clientPromise from "../../../backend/lib/mongodb";
 import { randomBytes, randomUUID } from "crypto";
 import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
-  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
       clientId: '31528317457-g0ic4nuk1hjadv7tn39aha6d5hodgm3e.apps.googleusercontent.com',
