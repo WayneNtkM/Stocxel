b0597790380f05c5df08dea97edca04f74c3e252
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-02-02 12:53:38 -0300

    feat: MongoDB connection - MSC

---
+++ src/backend/services/User.service.ts
@@ -0,0 +1,22 @@
+import { FilterQuery } from 'mongoose';
+import User, { UserDocument } from '../models/User.model';
+import * as bcrypt from 'bcrypt';
+
+export default class Userservice {
+  public static async createUser(input: UserDocument) {
+    return User.create(input);
+  }
+
+  public static async findOneUser(input: FilterQuery<UserDocument>, options = { lean: true }) {
+    return User.findOne(input, null, options);
+  }
+
+  public static async loginUser({ email, password }: { email: UserDocument['email']; password: UserDocument['password']; }) {
+    const user = await Userservice.findOneUser({ email }, { lean: true });
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      throw new Error('User does not exist');
+    }
+    return user;
+  }
+}
